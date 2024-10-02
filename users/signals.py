be883d535ad8a6cf4535a5b69ab0ded5ca600d0e
addeed signals <nurelmalabaev95@gmail.com>
2024-10-02 15:45:46 +0600

    Profile

--- users/signals.py
+++ users/signals.py
@@ -4,9 +4,9 @@ from django.dispatch import receiver
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
-        profile = Profile(user=instance)
-@post_save(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
     instance.profile.save()
---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'register.html',{'form': form})
+
+def profile(request):
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+    context = {'u_form': u_form, 'p_form': p_form}
+    return render(request,'profile.html',context)
